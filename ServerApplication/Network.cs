3f35099ce1a3f2d486cc03c388ef2e3e50ecaea5
Bowen Tian <bowen.tian@mail.mcgill.ca>
2018-10-06 16:21:04 -0400

    Server network connection
    Server network connection

---
+++ ServerApplication/Network.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerApplication
+{
+    class Network
+    {
+        public TcpListener ServerSocket;
+        public static Network instance = new Network();
+        public static Client[] Clients = new Client[100];
+
+        public void ServerStart()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Clients[i] = new Client();
+            }
+
+            ServerSocket = new TcpListener(IPAddress.Any,5500);
+            ServerSocket.Start();
+            ServerSocket.BeginAcceptTcpClient(OnClientConnect, null);
+            Console.WriteLine("Server has successfully started.");
+        }
+
+        void OnClientConnect(IAsyncResult result)
+        {
+            TcpClient client = ServerSocket.EndAcceptTcpClient(result);
+            client.NoDelay = false;
+            ServerSocket.BeginAcceptTcpClient(OnClientConnect, null);
+
+
+        }
+    }
+}
--- ServerApplication/Program.cs
+++ ServerApplication/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ServerApplication
 {
     class Program
     {
+        private static Thread threadConsole;
+        private static bool consoleRunning;
+
         static void Main(string[] args)
         {
+            threadConsole = new Thread(new ThreadStart(ConsoleThread));
+            threadConsole.Start();
+            Network.instance.ServerStart();
+        }
+
+        private static void ConsoleThread()
+        {
+            string line;
+            consoleRunning = true;
+
+            while (consoleRunning)
+            {
+                line = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    consoleRunning = false;
+                    return;
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
